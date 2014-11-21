4e6a0275d7208ecddf99dbd0808efb53671590a9
Sorskoot <mental_c@hotmail.com>
2014-11-21 19:43:36 +0100

    fixed sharetarger

---
+++ ToDoMVC/js/services/shareTarget.js
@@ -0,0 +1,26 @@
+﻿/*global define*/
+'use strict';
+
+define(['angular', 'app', 'controllers/todo','services/todoStorage'], function (angular, app) {
+
+    app.factory('shareTarget', function (todoStorage) {
+        return {
+            share: function (shareOperation) {                
+                if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text)) {
+                    shareOperation.data.getTextAsync().done(function (text) {
+                        var todos = todoStorage.get();
+
+                        todos.push({
+                            title: text,
+                            completed: false
+                        });
+
+                        todoStorage.put(todos);
+                        shareOperation.reportCompleted();
+                    });
+
+                }
+            }
+        }
+    })
+});
